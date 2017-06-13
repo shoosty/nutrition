fa665b4a94e9c99114f0306da6dff026403afc6b
Jesus Castello <matugm@gmail.com>
2017-06-13 16:04:43 +0200

    Remove old comments

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,4 @@
-# Dir.pwd
+
 require 'active_record'
 require 'minitest/autorun'
 require 'table_print'
@@ -18,7 +18,6 @@ class Deriv_Cd < ActiveRecord::Base
 end
 
 class FoodGroup  < ActiveRecord::Base
-  # works with table food_groups
   has_many :food_descriptions, foreign_key: :FdGrp_Cd, primary_key: :FdGrp_Cd
 end
 
@@ -26,18 +25,15 @@ class FoodDescription  < ActiveRecord::Base
   has_one :food_group, foreign_key: :FdGrp_Cd, primary_key: :FdGrp_Cd
   has_many :nut_data, foreign_key: :NDB_No, primary_key: :NDB_No
 
-# both of these methods change the representation of hte object only when printing. Optional.
-  def to_s   # puts
+  def to_s
     self.Shrt_Desc
   end
 
-  def inspect   # when you use p it uses inspect
-    #without the self it thinks it is a constant becuase it starts with upper case. To compensate the self is added to specifically tell ruby it is a method and not a constant.
+  def inspect
     self.Long_Desc
   end
 end
-# the symbol after the has_one example :food_group is the method.
-#........................................
+
 class NutDatum < ActiveRecord::Base
   has_one :food_description, foreign_key: :NDB_No, primary_key: :NDB_No
   has_one :nutr_def, foreign_key: :Nutr_No, primary_key: :Nutr_No
@@ -47,7 +43,7 @@ class NutData < ActiveRecord::Base
   has_one :food_description, foreign_key: :NDB_No, primary_key: :NDB_No
   has_one :nutr_def, foreign_key: :Nutr_No, primary_key: :Nutr_No
 end
-#......................................
+
 class NutrDef < ActiveRecord::Base
   has_many :nut_data, foreign_key: :Nutr_No, primary_key: :Nutr_No
 end
@@ -71,11 +67,10 @@ fd = FoodDescription.new
 fg = FoodGroup.new
 
 def test
-  # test examples using related tables
   FoodDescription.first.food_group
   FoodDescription.last.food_group
-  FoodGroup.first.food_description   # this only works with has_one
-  tp FoodGroup.first.food_descriptions  # notice the plaural realted to has_many.
+  FoodGroup.first.food_description
+  tp FoodGroup.first.food_descriptions
   tp FoodGroup.all[1].food_descriptions
   tp FoodDescription.where "Long_Desc like '%Bacon%'"
   puts FoodDescription.select("Long_Desc").where("Long_Desc like '%Bacon%'").last
